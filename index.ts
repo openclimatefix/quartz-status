1ed713fd75ec78a24cf00055212a3db2e45e2e8d
braddf <bradbdf@gmail.com>
2024-01-25 15:56:11 +0000

    add eslint, prettier, husky

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { Instance } from "express-ws";
+import express_ws, { Instance } from "express-ws";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const expressWs: Instance = require('express-ws')(app);
+const expressWs: Instance = express_ws(app);
 
 app.use(function (req, res, next) {
-  console.log('middleware');
+  console.log("middleware");
   // req.testing = 'testing';
   return next();
 });
@@ -19,12 +19,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-expressWs.app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
+expressWs.app.ws("/", function (ws) {
+  ws.on("message", function (msg) {
     console.log(msg);
   });
-  ws.on('connection', function(connection) {
-    console.log('Connected')
+  ws.on("connection", function (connection) {
+    console.log("Connected");
     console.log(connection);
   });
   // console.log('socket', req.testing);
